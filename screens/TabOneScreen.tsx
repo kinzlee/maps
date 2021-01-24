9b9032e79db16a5a6b04b1ed5f140a14422f7702
kinzlee <chinemeremnwoga@gmail.com>
2021-01-24 21:36:33 +0100

    fixes

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,36 +1,83 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-import Animated from 'react-native-reanimated'
+import React from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import Slide from "../components/fluidTestComponent/Slide";
 
+const { width } = Dimensions.get("window");
 
+const slides = [
+  {
+    color: "#3984FF",
+    picture: require("../assets/images/tstImg.png"),
+    aspectRatio: 439.75 / 470.5,
+  },
+  {
+    color: "#39ffb4",
+    picture: require("../assets/images/tstImg.png"),
+    aspectRatio: 400.5 / 429.5,
+  },
+  {
+    color: "#ffb439",
+    picture: require("../assets/images/tstImg.png"),
+    aspectRatio: 391.25 / 520,
+  },
+];
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    width,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-const About  = ({navigation}:any) => {
-
-  const slides = [
-    {
-      color: "#3984FF",
-      picture: require('../assets/images/tstImg.png'),
-      aspectRatio: 439.7/470.5
+const Fluid = () => {
+  const x = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
     },
-    {
-      color: "#3984FF",
-      picture: require('../assets/images/tstImg.png'),
-      aspectRatio: 439.7/470.5
-    },
-    {
-      color: "#3984FF",
-      picture: require('../assets/images/tstImg.png'),
-      aspectRatio: 439.7/470.5
-    }
-  ]
-
-
+  });
   return (
-      <Animated.ScrollView>
-        {slides.map((slide, index) => <Slide slide={slide} key={index} index={index} />)}
+    <View style={styles.root}>
+      <Animated.ScrollView
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+        snapToInterval={width}
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      >
+        {slides.map((slide, index) => {
+          const isFirst = index === 0;
+          const isLast = index === slides.length - 1;
+          return (
+            <View key={index} style={styles.container}>
+              <Slide
+                x={x}
+                index={index}
+                aspectRatio={slide.aspectRatio}
+                picture={slide.picture}
+                colors={[
+                  isFirst ? slide.color : slides[index - 1].color,
+                  slide.color,
+                  isLast ? slide.color : slides[index + 1].color,
+                ]}
+              />
+            </View>
+          );
+        })}
       </Animated.ScrollView>
-  )
-}
+    </View>
+  );
+};
 
 export default About
 
